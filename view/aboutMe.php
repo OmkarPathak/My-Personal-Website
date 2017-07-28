4771fb3645003f427f9d5195cd6c93c10e339b9c
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-28 07:05:44 +0530

    Modified view

--- view/aboutMe.php
+++ view/aboutMe.php
@@ -2,49 +2,242 @@
   include('../includes/headerForPosts.php');
 ;?>
       
-      <div class="container">
-        <p style="font-size: 25px;" class="text-center">Now that you have shown interest in knowing me, let me tell you something about me.</p>
-        <div class="row">
-          <p style="font-size: 20px;">I love programming and get this straight! I like to code. I hate to attend lectures. Yeah that's right, teachers are pissed at me all time because of this. <i>But guess what, I am ok with that!</i> I am into competitive program from about 2 months as on June 2017. Competitive programming boosts your programming skills to other level. Ah! I hate to write about myself. If you want to know more just find me on Quora and read my answers. </p>
-
-          <p style="font-size: 20px;">Here are some of my pictures (Some recent, some old)</p>
-          
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 thumbnail">
-                <div class="panel panel-default text-center slideanim">
-                    <img src="../images/Omkar1.jpeg" class="img-responsive">
-                 </div> <!-- ./panel -->
-          </div><!-- ./column -->
-
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 thumbnail">
-                <div class="panel panel-default text-center slideanim">
-                    <img src="../images/Omkar2.jpeg" class="img-responsive">
-                 </div> <!-- ./panel -->
-          </div><!-- ./column -->
-
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 thumbnail">
-                <div class="panel panel-default text-center slideanim">
-                    <img src="../images/Omkar3.jpeg" class="img-responsive">
-                 </div> <!-- ./panel -->
-          </div><!-- ./column -->
-
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 thumbnail">
-                <div class="panel panel-default text-center slideanim">
-                    <img src="../images/Omkar4.jpeg" class="img-responsive">
-                 </div> <!-- ./panel -->
-          </div><!-- ./column -->
-
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 thumbnail">
-                <div class="panel panel-default text-center slideanim">
-                    <img src="../images/Omkar5.jpeg" class="img-responsive">
-                 </div> <!-- ./panel -->
-          </div><!-- ./column -->
-
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 thumbnail">
-                <div class="panel panel-default text-center slideanim">
-                    <img src="../images/Omkar6.jpeg" class="img-responsive">
-                 </div> <!-- ./panel -->
-          </div><!-- ./column -->
-        </div>
+      <div class="header text-center">
+        <h1>SOME OF MY ACHIEVEMENTS!</h1>
+        <h4>-Because everyone has a unique story..</h4>
       </div>
 
+      <section id="highlights" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container">
+          <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim">
+                    <h2 class="section-heading">HIGHLIGHTS</h2>
+                    <hr style="max-width:50px;border-top: 2px solid #9868C6;"><br>
+                </div><!-- ./column -->
+          </div><!-- ./row -->
+          
+          <div class="row" style="margin: 0">
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 slideanim nochange">
+              <div class="col-md-2 col-lg-2 col-sm-12 col-xs-12 text-center">
+                <i class="fa fa-trophy" style="font-size:40px; color: #9868C6"></i>
+              </div>
+              <div class="col-md-10 col-lg-10 col-sm-12 col-xs-12">
+                <label><b>ACADEMICS</b></label><br>
+                <p>Marks, certifications, technologies used.</p>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 slideanim">
+              <div class="col-md-2 col-lg-2 col-sm-12 col-xs-12 text-center">
+                <i class="fa fa-cogs" style="font-size:40px; color: #9868C6"></i>
+              </div>
+              <div class="col-md-10 col-lg-10 col-sm-12 col-xs-12">
+                <label><b>PROJECTS</b></label><br>
+                <p>The great aim of Education is not Knowledge, but Action</p>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 slideanim">
+              <div class="col-md-2 col-lg-2 col-sm-12 col-xs-12 text-center">
+                <i class="fa fa-industry" style="font-size:40px; color: #9868C6"></i>
+              </div>
+              <div class="col-md-10 col-lg-10 col-sm-12 col-xs-12">
+                <label><b>INTERNSHIPS</b></label><br>
+                <p>Because, experience is all that matters</p>
+              </div>
+            </div>
+          </div> <!-- ./row -->
+      </section><!-- ./section-achievements -->
+
+      <section id="education" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container" style="background-color: #efefef">
+          <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim nochange">
+                    <h2 class="section-heading">EDUCATION</h2>
+                    <hr style="max-width:50px;border-top: 2px solid #9868C6;"><br>
+                </div><!-- ./column -->
+          </div><!-- ./row -->
+          
+          <div class="row" style="margin: 0">
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 slideanim">
+              <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center">
+                <i class="fa fa-graduation-cap" style="font-size:40px; color: darkgray"></i>
+              </div>
+              <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center">
+                <label style="padding-top: 10px;"><b>Marathwada Mitra Mandal's College of Engineering</b></label><br>
+                <div>
+                  <p>Duration: 2014 - Present</p>
+                  <p>Course: BE (Computer Engineering)</p>
+                  <p>University: Pune University</p>
+                  <p>Marks: <b>73.4%</b> (as of Sixth Semester)</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 slideanim">
+              <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center">
+                <i class="fa fa-university" style="font-size:40px; color: darkgray"></i>
+              </div>
+              <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center">
+                <label style="padding-top: 10px;"><b>Sinhgad College of Science</b></label><br>
+                <div>
+                  <p>Duration: 2012 - 2014</p>
+                  <p>Board: Class 12<sup>th</sup> HSC</p>
+                  <p>Marks: <b>78%</b></p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 slideanim">
+              <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center">
+                <i class="fa fa-home" style="font-size:40px; color: darkgray"></i>
+              </div>
+              <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center">
+                <label style="padding-top: 10px;"><b>Dnyanganga English Medium School</b></label><br>
+                <div>
+                  <p>Duration: 2002 - 2012</p>
+                  <p>Board: Class 10<sup>th</sup> SSC</p>
+                  <p>Marks: <b>89.81%</b></p>
+                  <p><b>School topper in Marathi language</b></p>
+                </div>
+              </div>
+            </div>
+          </div> <!-- ./row -->
+      </section><!-- ./section-education -->
+
+       <section id="projects" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container">
+        <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim nochange">
+                  <h2 class="section-heading">PROJECTS</h2>
+                  <hr style="max-width:50px;border-top: 2px solid #9868C6;"><br>
+              </div><!-- ./column -->
+        </div><!-- ./row -->
+        
+        <div class="row" style="margin: 0">
+          <div class="col-md-4 col-lg-4 col-sm-12 co-xs-12 text-center block slideanim">
+            <label><b>Smart Surveillance System using Raspberry Pi and Face recognition</b></label>
+            <p class="text-justify" style="padding-top: 10px;">A Surveillance system created using IoT for sending alerts to authenticated when any unauthenticated person enters your premises</p>
+            <a href="https://github.com/OmkarPathak/Smart-Surveillance-System-using-Raspberry-Pi"><button class="btn btn-lg">View</button></a>
+          </div>
+
+          <div class="col-md-4 col-lg-4 col-sm-12 co-xs-12 text-center block slideanim">
+            <label><b>Password Strength evaluator using Machine Learning</b></label>
+            <p class="text-justify" style="padding-top: 10px;">Simple password evaluator which tells you the strength of your password based on previously generated dataset.</p>
+            <a href="https://github.com/OmkarPathak/Password-Strength-Evaluator-using-Machine-Learning"><button class="btn btn-lg">View</button></a>
+          </div>
+
+          <div class="col-md-4 col-lg-4 col-sm-12 co-xs-12 text-center block slideanim">
+            <label><b>Personal Website (Created because of keen interest in web development)</b></label>
+            <p class="text-justify" style="padding-top: 10px;">This website, which is created using Full Stack web development techniques such as PHP, MySQL, HTML, CSS and JS</p>
+            <a href="https://github.com/OmkarPathak/My-Personal-Website"><button class="btn btn-lg">View</button></a>
+          </div>
+        </div> <!-- ./row -->
+
+        <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim nochange" style="margin-top: 20px;">
+                  <h2 class="section-heading">MINI PROJECTS</h2>
+                  <hr style="max-width:50px;border-top: 2px solid #9868C6;"><br>
+              </div><!-- ./column -->
+        </div><!-- ./row -->
+
+        <div class="row">
+          <div class="col-md-4 col-lg-4 col-sm-12 co-xs-12 text-center block slideanim">
+            <label><b>A reminder application to remind you about the upcoming birthdays</b></label>
+            <p class="text-justify" style="padding-top: 10px;">Forget birthdays of your loved ones? Don't worry its time to wish them all. This script will remind you of the birthdays that you have already stored.</p>
+            <a href="https://github.com/OmkarPathak/Python-Programs/blob/master/Scripts/P09_ReminderApplication.py"><button class="btn btn-lg">View</button></a>
+          </div>
+
+          <div class="col-md-4 col-lg-4 col-sm-12 co-xs-12 text-center block slideanim">
+            <label><b>Download all CodeChef Problem statements</b></label>
+            <p class="text-justify" style="padding-top: 10px;">Are you into Competitive programming like me? Then you must be storing your solved questions too. This script will help you to download all the codechef problem statements with ease.</p>
+            <a href="https://github.com/OmkarPathak/Codechef-Problem-Statement-Downloader"><button class="btn btn-lg">View</button></a>
+          </div>
+
+          <div class="col-md-4 col-lg-4 col-sm-12 co-xs-12 text-center block slideanim">
+            <label><b>A simple note taking application in Python</b></label>
+            <p class="text-justify" style="padding-top: 10px;">Worried about forgetting something? Try this app. It creates, reads, updates notes that you create. Best part? If you specify the time, it will automatically send you an email about the reminder.</p>
+            <a href="https://github.com/OmkarPathak/A-Simple-Note-Taking-Terminal-App"><button class="btn btn-lg">View</button></a>
+        </div>
+        </div> <!-- ./row -->
+      </section> <!-- ./section-projects -->
+
+      <section id="technologies" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container" style="background-color: #efefef">
+          <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim nochange">
+                    <h2 class="section-heading">TECHNOLOGIES</h2>
+                    <hr style="max-width:50px; border-top: 2px solid #9868C6; margin-bottom: 0"><br>
+                    <p style="margin-top: 0;"><i>These are the technologies I have worked with throughout my curriculum</i></p>
+                </div><!-- ./column -->
+          </div><!-- ./row -->
+          
+          <div class="row" style="margin-top: 20px;">
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>Python</b></label>
+              <p class="text-justify">I am a Python enthusiast who loves to code in Python. I have completed several mini projects using Python, Django and Flask.</p>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>Web Development</b></label>
+              <p class="text-justify">PHP is the language in which this website is built on. I have also worked with MySQL, HTML, CSS, JavaScript, Ajax and JQuery.
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>Bootstrap</b></label>
+              <p class="text-justify">This saves a lot of my time! I regularly use Bootstrap framework for my front-end projects.</p>
+            </div>
+          </div> <!-- ./row -->
+
+          <div class="row" style="margin-top: 20px;">
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>Linux</b></label>
+              <p class="text-justify">499 of 500 super computers in the world run on Linux. Rest is self-explanatory.</p>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>GitHub</b></label>
+              <p class="text-justify">I am a huge fan of Git version control system. I love to contribute to open source projects. It's like Yoga for programmers.</p>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>C/ C++</b></label>
+              <p class="text-justify">Okay, I am not a huge fan of C and C++ - but where the Python speed fails - C and C++ come to my rescue.</p>
+            </div>
+          </div> <!-- ./row -->
+      </section><!-- ./section-education -->
+
+      <section id="internships" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container">
+          <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim nochange">
+                    <h2 class="section-heading">INTERNSHIPS</h2>
+                    <hr style="max-width:50px; border-top: 2px solid #9868C6; margin-bottom: 0"><br>
+                    <p style="margin-top: 0;"><i>Working Experience</i></p>
+                </div><!-- ./column -->
+          </div><!-- ./row -->
+
+          <div class="row">
+            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>SoftTestLab Technologies (June-17 - July-17)</b></label>
+              <p style="padding-left: 30px; padding-right: 30px;">Worked as a Web Developer intern. Mostly worked on creating reports using PHP and Excel. Was responsible for making a live project for software testing purposes for the company, which are created using Laravel framework.</p>
+            </div>
+
+            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 text-center slideanim">
+              <label><b>GeeksForGeeks (August-17 - Present)</b></label>
+              <p style="padding-left: 30px; padding-right: 30px;">Working as technical writer for various topics such as Data Structures and Algorithms, Python. Have already published 4 articles.</p>
+            </div>
+          </div> <!-- ./row -->
+      </section> <!-- ./section-internship -->
+
+      <section id="resume" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container" style="background-color: #efefef">
+          <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slideanim nochange">
+                    <h2 class="section-heading">RESUME</h2>
+                    <hr style="max-width:50px;border-top: 2px solid #9868C6;"><br>
+                </div><!-- ./column -->
+          </div><!-- ./row -->
+          
+          <div class="row" style="margin: 0">
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 slideanim text-center">
+              <a href="OmkarResume.pdf"><i class="fa fa-file-pdf-o" style="font-size:40px;"></i><p>Resume</p></a>
+            </div>
+          </div> <!-- ./row -->
+      </section> <!-- ./section-resume -->
+      
 <?php  include('../includes/footer.php');?>
